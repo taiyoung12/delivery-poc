0577d36de984fc7272ee15c6ff3d4c48a5d45c06
taiyoung <qq3222@naver.com>
2025-04-01 23:17:20 +0900

    Feature: Properties Configuration Setting

---
+++ src/main/java/com/barogo/java/delivery/poc/common/config/app/PropertiesConfiguration.java
@@ -0,0 +1,9 @@
+package com.barogo.java.delivery.poc.common.config.app;
+
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("com.barogo.java.delivery.poc")
+public class PropertiesConfiguration {
+}
