705b0eb212a9c2a196ebc0cdc136c12f4783ffb4
taiyoung <qq3222@naver.com>
2025-04-02 21:08:58 +0900

    Feature: JwtGenerator

--- src/main/java/com/barogo/java/delivery/poc/common/config/token/JwtGenerator.java
+++ src/main/java/com/barogo/java/delivery/poc/common/config/token/JwtGenerator.java
@@ -1,18 +1,30 @@
 package com.barogo.java.delivery.poc.common.config.token;
 
+import java.security.Key;
 import java.util.Date;
 
 import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 
-public class JwtGenrator {
-	private final String secret = "your-secret-key";
-	private final long expiration = 1000 * 60 * 60L;
+public class JwtGenerator {
+	private final JwtProperties jwtProperties;
+	private final Key secretKey;
 
-	public String generateToken(String userId) {
+	public JwtGenerator(
+		JwtProperties jwtProperties
+	) {
+		this.jwtProperties = jwtProperties;
+		this.secretKey = Keys.hmacShaKeyFor(jwtProperties.getSecretKey().getBytes());
+	}
+
+	public String generateAccessToken(String subject) {
+		long now = System.currentTimeMillis();
 		return Jwts.builder()
-			.setSubject(userId)
-			.setIssuedAt(new Date())
-			.setExpiration(new Date(System.currentTimeMillis() + expiration))
+			.setSubject(subject)
+			.setIssuedAt(new Date(now))
+			.setExpiration(new Date(now + jwtProperties.getAccessTokenExpiration()))
+			.signWith(secretKey, SignatureAlgorithm.HS256)
 			.compact();
 	}
 }
