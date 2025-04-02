3319b7d36284eef4da163c31b0295a617198986e
taiyoung <qq3222@naver.com>
2025-04-02 21:10:47 +0900

    Feature: JwtValidator

--- src/main/java/com/barogo/java/delivery/poc/common/config/token/JwtProperties.java
+++ src/main/java/com/barogo/java/delivery/poc/common/config/token/JwtProperties.java
@@ -1,2 +1,14 @@
-package com.barogo.java.delivery.poc.common.config.token;public class JwtProperties {
+package com.barogo.java.delivery.poc.common.config.token;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@Getter
+@RequiredArgsConstructor
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+	private final String secretKey;
+	private final Long accessTokenExpiration;
 }
