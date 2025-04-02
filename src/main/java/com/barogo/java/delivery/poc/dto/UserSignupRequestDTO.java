f4046c48526093c7f6061b4f1f5f7bc449d8c7a1
taiyoung <qq3222@naver.com>
2025-04-02 22:56:53 +0900

    Feature: User Singup Request DTO

--- src/main/java/com/barogo/java/delivery/poc/dto/UserSignupRequestDTO.java
+++ src/main/java/com/barogo/java/delivery/poc/dto/UserSignupRequestDTO.java
@@ -1,2 +1,21 @@
-package com.barogo.java.delivery.poc.dto;public class UserSignupRequestDTO {
+package com.barogo.java.delivery.poc.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UserSignupRequestDTO {
+
+	@NotBlank(message = "이메일은 필수입니다.")
+	private String email;
+
+	@NotBlank(message = "비밀번호는 필수입니다.")
+	@Size(min = 12, message = "비밀번호는 최소 12자 이상이어야 합니다.")
+	private String password;
+
+	@NotBlank(message = "이름은 필수입니다.")
+	private String name;
 }
