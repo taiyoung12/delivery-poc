2160aaa0b55fc32b38bd7263e56253c386fb1cc4
taiyoung <qq3222@naver.com>
2025-04-02 23:20:15 +0900

    Feature: UserCreator Service Layer

--- src/main/java/com/barogo/java/delivery/poc/service/UserCreator.java
+++ src/main/java/com/barogo/java/delivery/poc/service/UserCreator.java
@@ -1,2 +1,24 @@
-package com.barogo.java.delivery.poc.service;public class UserCreator {
+package com.barogo.java.delivery.poc.service;
+
+import com.barogo.java.delivery.poc.dto.UserSignupRequestDTO;
+import com.barogo.java.delivery.poc.entity.User;
+import com.barogo.java.delivery.poc.repository.UserRepository;
+
+public class UserCreator {
+
+	private final UserRepository userRepository;
+
+	public UserCreator(UserRepository userRepository) {
+		this.userRepository = userRepository;
+	}
+
+	public void signUp(UserSignupRequestDTO requestDTO) {
+		User user = User.builder()
+			.email(requestDTO.getEmail())
+			.password(requestDTO.getPassword())
+			.name(requestDTO.getName())
+			.build();
+
+		User savedUer = userRepository.save(user);
+	}
 }
---
+++ src/test/java/com/barogo/java/delivery/poc/service/UserCreatorTest.java
@@ -0,0 +1,46 @@
+package com.barogo.java.delivery.poc.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.barogo.java.delivery.poc.dto.UserSignupRequestDTO;
+import com.barogo.java.delivery.poc.entity.User;
+import com.barogo.java.delivery.poc.repository.UserRepository;
+
+@ExtendWith(MockitoExtension.class)
+public class UserCreatorTest {
+
+	@Mock
+	private UserRepository userRepository;
+
+	private UserCreator sut;
+
+	@BeforeEach
+	void setUp() {
+		sut = new UserCreator(userRepository);
+	}
+
+	@Test
+	void requestDTO가_정상으로_들어오면_userRepository를_호출할_수_있다(){
+		String email = "barogo@barogo.com";
+		String password = "barogo1234";
+		String name = "바로고";
+
+		UserSignupRequestDTO requestDTO = new UserSignupRequestDTO();
+		requestDTO.setEmail(email);
+		requestDTO.setPassword(password);
+		requestDTO.setName(name);
+
+		when(userRepository.save(any())).thenReturn(any());
+
+		sut.signUp(requestDTO);
+
+		verify(userRepository, times(1)).save(any());
+	}
+}
