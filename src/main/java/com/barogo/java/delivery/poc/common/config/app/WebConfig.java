e06b125752e2c960cdb28a0dd0a79e53c5468d31
taiyoung <qq3222@naver.com>
2025-04-02 21:27:39 +0900

    Feature: JwtInterceptor

--- src/main/java/com/barogo/java/delivery/poc/common/config/app/WebConfig.java
+++ src/main/java/com/barogo/java/delivery/poc/common/config/app/WebConfig.java
@@ -1,2 +1,28 @@
-package com.barogo.java.delivery.poc.common.config.app;public class WebConfig {
+package com.barogo.java.delivery.poc.common.config.app;
+
+import static com.barogo.java.delivery.poc.common.config.app.path.ApiPath.Auth.*;
+import static com.barogo.java.delivery.poc.common.config.app.path.ApiPath.Delivery.*;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import com.barogo.java.delivery.poc.common.interceptor.JwtInterceptor;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+	private final JwtInterceptor jwtInterceptor;
+
+	public WebConfig(JwtInterceptor jwtInterceptor) {
+		this.jwtInterceptor = jwtInterceptor;
+	}
+
+	@Override
+	public void addInterceptors(InterceptorRegistry registry) {
+		registry.addInterceptor(jwtInterceptor)
+			.addPathPatterns(DELIVERY_ALL)
+			.excludePathPatterns(AUTH_ALL);
+	}
 }
+
--- src/main/java/com/barogo/java/delivery/poc/common/config/app/path/ApiPath.java
+++ src/main/java/com/barogo/java/delivery/poc/common/config/app/path/ApiPath.java
@@ -1,2 +1,11 @@
-package com.barogo.java.delivery.poc.common.config.app.path;public class ApiPath {
+package com.barogo.java.delivery.poc.common.config.app.path;
+
+public class ApiPath {
+	public static class Auth {
+		public static final String AUTH_ALL = "/api/v1/auth/**";
+	}
+
+	public static class Delivery {
+		public static final String DELIVERY_ALL = "/api/v1/delivery/**";
+	}
 }
