5398c0955613d9f6c8143e434b91ca296e326633
taiyoung <qq3222@naver.com>
2025-04-03 20:53:38 +0900

    Feature: Delivery Updater Service layer

--- src/main/java/com/barogo/java/delivery/poc/service/DeliveryUpdater.java
+++ src/main/java/com/barogo/java/delivery/poc/service/DeliveryUpdater.java
@@ -1,2 +1,23 @@
-package com.barogo.java.delivery.poc.service;public class DeliveryUpdator {
+package com.barogo.java.delivery.poc.service;
+
+import org.springframework.stereotype.Service;
+
+import com.barogo.java.delivery.poc.entity.Delivery;
+import com.barogo.java.delivery.poc.repository.DeliveryRepository;
+
+@Service
+public class DeliveryUpdater {
+	private final DeliveryRepository deliveryRepository;
+
+	public DeliveryUpdater(DeliveryRepository deliveryRepository) {
+		this.deliveryRepository = deliveryRepository;
+	}
+
+	void updateDelivery(
+		Delivery delivery,
+		String address
+	){
+		delivery.setAddress(address);
+		deliveryRepository.save(delivery);
+	}
 }
