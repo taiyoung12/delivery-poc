607681b2e4090f82eafb5fc4f0a112cdd5cb8f29
taiyoung <qq3222@naver.com>
2025-04-03 16:03:34 +0900

    Feature: PasswordConfig Setting

---
+++ src/main/java/com/barogo/java/delivery/poc/common/config/app/PasswordConfig.java
@@ -0,0 +1,14 @@
+package com.barogo.java.delivery.poc.common.config.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+public class PasswordConfig {
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+}
