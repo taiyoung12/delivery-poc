7bf5b26f5cc369ba5d0d602faa32a3a0171b41be
taiyoung <qq3222@naver.com>
2025-04-03 20:01:31 +0900

    Feature: UserReader Transactional annotation'

--- src/main/java/com/barogo/java/delivery/poc/service/UserReader.java
+++ src/main/java/com/barogo/java/delivery/poc/service/UserReader.java
@@ -3,6 +3,7 @@ package com.barogo.java.delivery.poc.service;
 import static com.barogo.java.delivery.poc.common.code.UserCode.*;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.barogo.java.delivery.poc.common.exception.BaseException;
 import com.barogo.java.delivery.poc.entity.User;
@@ -16,6 +17,7 @@ public class UserReader {
 		this.userRepository = userRepository;
 	}
 
+	@Transactional
 	public User findByEmail(String email){
 		return userRepository.findByEmail(email)
 			.orElseThrow(() -> new BaseException(NOT_FOUND_USER_BY_EMAIL));
