e070b490acf1f626f703a1d00041e6765a0688a4
taiyoung <qq3222@naver.com>
2025-04-03 21:44:48 +0900

    Test: Delivery Controller update

--- src/test/java/com/barogo/java/delivery/poc/controller/DeliveryControllerTest.java
+++ src/test/java/com/barogo/java/delivery/poc/controller/DeliveryControllerTest.java
@@ -1,5 +1,6 @@
 package com.barogo.java.delivery.poc.controller;
 
+import static com.barogo.java.delivery.poc.common.code.DeliveryCode.*;
 import static org.mockito.Mockito.*;
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.*;
@@ -7,6 +8,7 @@ import static org.springframework.restdocs.payload.PayloadDocumentation.*;
 import static org.springframework.restdocs.request.RequestDocumentation.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 
@@ -27,15 +29,21 @@ import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import com.barogo.java.delivery.poc.common.exception.BaseException;
 import com.barogo.java.delivery.poc.common.exception.GlobalExceptionHandler;
+import com.barogo.java.delivery.poc.dto.request.DeliveryUpdateRequest;
 import com.barogo.java.delivery.poc.dto.response.DeliveryReadResponse;
 import com.barogo.java.delivery.poc.entity.User;
+import com.barogo.java.delivery.poc.service.delivery.DeliveryFacade;
 import com.barogo.java.delivery.poc.service.delivery.DeliveryReader;
 import com.barogo.java.delivery.poc.service.user.UserReader;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ExtendWith({RestDocumentationExtension.class, MockitoExtension.class})
 public class DeliveryControllerTest {
+
+	@Mock
+	private DeliveryFacade deliveryFacade;
 	@Mock
 	private DeliveryReader deliveryReader;
 
@@ -132,6 +140,159 @@ public class DeliveryControllerTest {
 					fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
 					fieldWithPath("data").type(JsonFieldType.OBJECT).description("유효성 에러 상세 정보"),
 					fieldWithPath("data.validDateRange").type(JsonFieldType.STRING).description("날짜 유효성 검사 실패 메시지")
+				)
+			));
+	}
+
+	@Test
+	void 배송지_수정_요청이_성공하면_200_상태코드와_수정된_주소를_반환할_수_있다() throws Exception {
+		String email = "test@barogo.com";
+		Long deliveryId = 1L;
+		String newAddress = "서울시 마포구 서강로 123";
+
+		User user = User.builder().build();
+		ReflectionTestUtils.setField(user, "id", 1L);
+
+		DeliveryUpdateRequest request = new DeliveryUpdateRequest();
+		request.setDeliveryId(deliveryId);
+		request.setAddress(newAddress);
+
+		when(userReader.findByEmail(anyString())).thenReturn(user);
+		when(deliveryFacade.updateDeliveryAddress(user.getId(), deliveryId, newAddress))
+			.thenReturn(newAddress);
+
+		mockMvc.perform(put("/api/v1/delivery")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andExpect(status().isOk())
+			.andExpect(jsonPath("$.code").value("CM200"))
+			.andExpect(jsonPath("$.message").value("성공"))
+			.andExpect(jsonPath("$.data").value(newAddress))
+			.andDo(document("delivery-update",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("deliveryId").type(JsonFieldType.NUMBER).description("배송 ID"),
+					fieldWithPath("address").type(JsonFieldType.STRING).description("변경할 배송지 주소")
+				),
+				responseFields(
+					fieldWithPath("code").type(JsonFieldType.STRING).description("응답 코드"),
+					fieldWithPath("message").type(JsonFieldType.STRING).description("응답 메시지"),
+					fieldWithPath("data").type(JsonFieldType.STRING).description("수정된 배송지 주소")
+				)
+			));
+	}
+
+	@Test
+	void 존재하지_않는_배송ID면_404에러와_예외메시지를_반환할_수_있다() throws Exception {
+		Long userId = 1L;
+		Long deliveryId = 999L;
+		String newAddress = "서울시 송파구 문정동 102-1";
+
+		User user = User.builder().build();
+		ReflectionTestUtils.setField(user, "id", userId);
+
+		DeliveryUpdateRequest request = new DeliveryUpdateRequest();
+		request.setDeliveryId(deliveryId);
+		request.setAddress(newAddress);
+
+		when(userReader.findByEmail(anyString())).thenReturn(user);
+		when(deliveryFacade.updateDeliveryAddress(userId, deliveryId, newAddress))
+			.thenThrow(new BaseException(NOT_FOUND_DELIVERY_BY_ID));
+
+		mockMvc.perform(put("/api/v1/delivery")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andExpect(status().isNotFound())
+			.andExpect(jsonPath("$.code").value("DV404"))
+			.andExpect(jsonPath("$.message").value("배송 주문을 조회할 수 없습니다."))
+			.andDo(document("delivery-update-not-found",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("deliveryId").type(JsonFieldType.NUMBER).description("수정할 배송 ID"),
+					fieldWithPath("address").type(JsonFieldType.STRING).description("변경할 주소")
+				),
+				responseFields(
+					fieldWithPath("code").type(JsonFieldType.STRING).description("에러 코드"),
+					fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
+					fieldWithPath("data").type(JsonFieldType.STRING).description("에러 메시지")
+				)
+			));
+	}
+
+	@Test
+	void 주소_정합성_검증에_실패하면_404에러와_예외메시지를_반환할_수_있다() throws Exception {
+		Long userId = 1L;
+		Long deliveryId = 10L;
+		String newAddress = "!!!";
+
+		User user = User.builder().build();
+		ReflectionTestUtils.setField(user, "id", userId);
+
+		DeliveryUpdateRequest request = new DeliveryUpdateRequest();
+		request.setDeliveryId(deliveryId);
+		request.setAddress(newAddress);
+
+		when(userReader.findByEmail(anyString())).thenReturn(user);
+		when(deliveryFacade.updateDeliveryAddress(userId, deliveryId, newAddress))
+			.thenThrow(new BaseException(NOT_VALIDATED_ADDRESS));
+
+		mockMvc.perform(put("/api/v1/delivery")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andExpect(status().isNotFound())
+			.andExpect(jsonPath("$.code").value("DV404"))
+			.andExpect(jsonPath("$.message").value("주소 정합성 검증에 실패했습니다."))
+			.andDo(document("delivery-update-invalid-address",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("deliveryId").type(JsonFieldType.NUMBER).description("수정할 배송 ID"),
+					fieldWithPath("address").type(JsonFieldType.STRING).description("변경할 주소")
+				),
+				responseFields(
+					fieldWithPath("code").type(JsonFieldType.STRING).description("에러 코드"),
+					fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
+					fieldWithPath("data").type(JsonFieldType.STRING).description("에러 메시지")
+				)
+			));
+	}
+
+	@Test
+	void 본인의_배송이_아니라면_403에러와_예외메시지를_반환할_수_있다() throws Exception {
+		Long userId = 1L;
+		Long deliveryId = 20L;
+		String newAddress = "서울시 성동구 성수동 1가";
+
+		User user = User.builder().build();
+		ReflectionTestUtils.setField(user, "id", userId);
+
+		DeliveryUpdateRequest request = new DeliveryUpdateRequest();
+		request.setDeliveryId(deliveryId);
+		request.setAddress(newAddress);
+
+		when(userReader.findByEmail(anyString())).thenReturn(user);
+		when(deliveryFacade.updateDeliveryAddress(userId, deliveryId, newAddress))
+			.thenThrow(new BaseException(NOT_ALLOWED_UPDATE_DELIVERY_ADDRESS));
+
+		mockMvc.perform(put("/api/v1/delivery")
+				.contentType(MediaType.APPLICATION_JSON)
+				.content(objectMapper.writeValueAsString(request)))
+			.andExpect(status().isUnauthorized())
+			.andExpect(jsonPath("$.code").value("DV401"))
+			.andExpect(jsonPath("$.message").value("주소를 변경할 권한이 없습니다."))
+			.andDo(document("delivery-update-not-allowed",
+				preprocessRequest(prettyPrint()),
+				preprocessResponse(prettyPrint()),
+				requestFields(
+					fieldWithPath("deliveryId").type(JsonFieldType.NUMBER).description("수정할 배송 ID"),
+					fieldWithPath("address").type(JsonFieldType.STRING).description("변경할 주소")
+				),
+				responseFields(
+					fieldWithPath("code").type(JsonFieldType.STRING).description("에러 코드"),
+					fieldWithPath("message").type(JsonFieldType.STRING).description("에러 메시지"),
+					fieldWithPath("data").type(JsonFieldType.STRING).description("에러 메시지")
 				)
 			));
 	}
