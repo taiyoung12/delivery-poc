278b2c34bdccdafaccd27c9b30276138c2fca661
taiyoung <qq3222@naver.com>
2025-04-03 18:28:50 +0900

    Feature: User Login Controller

--- src/main/java/com/barogo/java/delivery/poc/dto/request/UserSigninRequest.java
+++ src/main/java/com/barogo/java/delivery/poc/dto/request/UserSigninRequest.java
@@ -1,2 +1,15 @@
-package com.barogo.java.delivery.poc.dto.request;public class UserSigninRequest {
+package com.barogo.java.delivery.poc.dto.request;
+
+import com.barogo.java.delivery.poc.dto.validation.ValidPassword;
+
+import jakarta.validation.constraints.NotBlank;
+import lombok.Getter;
+
+@Getter
+public class UserSigninRequest {
+	@NotBlank(message = "이메일은 필수입니다.")
+	private String email;
+
+	@NotBlank(message = "비밀번호는 필수입니다.")
+	private String password;
 }
