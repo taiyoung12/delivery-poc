b96ca641e13dd2b96bd60e2c9115c8d16f557a87
taiyoung <qq3222@naver.com>
2025-04-03 18:47:32 +0900

    Docs: User Login 문서화

--- src/main/java/com/barogo/java/delivery/poc/dto/request/UserSigninRequest.java
+++ src/main/java/com/barogo/java/delivery/poc/dto/request/UserSigninRequest.java
@@ -4,8 +4,10 @@ import com.barogo.java.delivery.poc.dto.validation.ValidPassword;
 
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
+import lombok.Setter;
 
 @Getter
+@Setter
 public class UserSigninRequest {
 	@NotBlank(message = "이메일은 필수입니다.")
 	private String email;
